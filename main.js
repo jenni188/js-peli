4ff2e220bbd13b884b169f9757d4d2230d88f3ba
Jenni Hynynen <jenni.hynynen@itvelhot.local>
2020-10-27 12:50:54 +0200

    toka commit

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 
-
-/*function setVolume() {
-    var mediaClip = document.getElementById("video1")
-    mediaClip.volume = document.getElementById("vol-control").value;
-}
-
-window.onload = function() {
-    // when  page is loaded restore your volume to it's save value
-    if (localStorage.getItem('volume') != null) {
-        var mediaClip = document.getElementById("video1")
-        mediaClip.volume = parseInt(localStorage.getItem('vol-control')); // you restore your volume setting to your save volume
-    }
-}
-function setVolume() {
-    var mediaClip = document.getElementById("video1");
-    mediaClip.volume = document.getElementById("vol-control").value;
-    localStorage.setItem('volume', mediaClip.volume); // you change your volume setting
-}
-*/
-$(document).ready(function(){
-    $(".img .volume-on").on('click' , function(){
+/*$(document).ready(function(){
+    $(".volume-btn .volume-on").on('click' , function(){
         $(this).hide();
         $(".volume-off").fadeIn();
         $("myMusic")[0].play();
     });
 });
+
+$(document).ready(function(){
+    $(".volume-btn .volume-off").on('click' , function(){
+        $(this).hide();
+        $(".volume-on").fadeIn();
+        $("myMusic")[0].pause();
+    });
+});
+*/
+//modal
+
+let modal = document.getElementById('shop-modal');
+
+function openModal(){
+    modal.style.display = "block";
+}
+
+function hideModal(){
+    modal.style.display = "none";
+}
